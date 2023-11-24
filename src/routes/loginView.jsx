af768e3d3a24a9e58ef34191ad3a274c86283652
VDelCastilloHm <ing.vitidelcastillo@gmail.com>
2023-11-24 16:57:06 -0500

    login view funcionando

--- src/routes/loginView.jsx
+++ src/routes/loginView.jsx
@@ -6,14 +6,14 @@ export default function LoginView(){
 async function handleOnClick(){
     const googleProvider = new GoogleAuthProvider();
     await signInWithGoogle(googleProvider)
-}
-
-async function signInWithGoogle(provider){
-    try {
-        const res = await signInWithPopup(auth,provider);
-        console.log(res);
-    } catch (error) {
-        console.error(error);
+    
+    async function signInWithGoogle(provider){
+        try {
+            const res = await signInWithPopup(auth,provider);
+            console.log(res);
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
